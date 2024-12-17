9bfe8e085d14213146abaf1a38094c1a3165b37c
Shriyans Kapoor <skapoor@junipersquare.com>
2024-12-17 10:16:36 +0530

    builk upload

--- generate-client.sh
+++ generate-client.sh
@@ -1,7 +1,7 @@
 npx @openapitools/openapi-generator-cli generate \
   -i openapi.yaml \
   -g typescript-axios \
-  -o ./typescript-client/ \
+  -o ./@jsq/main-openapi-axios/ \
   --generate-alias-as-model \
-  --additional-properties="npmName=@jsq/main-openapi-axios,npmVersion=10.0.0" \
+  --additional-properties="npmName=@jsq/main-openapi-axios,npmVersion=10.0.0,axiosVersion=^1.7.7" \
   -t ./updated_templates/
